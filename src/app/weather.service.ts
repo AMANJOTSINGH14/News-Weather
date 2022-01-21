f7d3948e6745762852fb32f5c7aba054523304f0
Amanjot Singh <singh_aman46@yahoo.com>
2022-01-21 17:56:27 +0530

    five commit

--- src/app/weather.service.ts
+++ src/app/weather.service.ts
@@ -9,7 +9,7 @@ import { tap } from 'rxjs/operators';
 export class WeatherService {
 weatherSubject=new BehaviorSubject<any>(null);
   constructor(private http:HttpClient) { }
-
+WeatherStart=new BehaviorSubject<any>(null);
 getWeatherData(location:any){
 return this.http.get(`http://api.weatherapi.com/v1/forecast.json?key=90d0cae69ea841fbbcb114918221001&q=${location}&days=10&aqi=no&alerts=no`).pipe(tap(v=>{
 console.log(v);
--- src/app/weather/weather.component.ts
+++ src/app/weather/weather.component.ts
@@ -1,4 +1,5 @@
 import { HttpClient } from '@angular/common/http';
+import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { WeatherService } from '../weather.service';
@@ -13,9 +14,24 @@ WeatherData:any
 isLoading=false
 loadedWeather:any
 isCard=false;
+
 constructor(private http:HttpClient,private weatherS:WeatherService,private router:Router) { }
 
   ngOnInit(): void {
+    this.weatherS.WeatherStart.subscribe(data=>{
+      console.log(data)
+      if(data!==null)
+      {this.isLoading=true
+        this.isCard=false;
+      console.log(data[0])
+      console.log(data[1])
+      this.WeatherData=data[0]
+      this.loadedWeather=data[1]
+      this.isLoading=false
+      this.isCard=true
+    }
+
+    })
   }
 onLocation(event:any){
 this.isLoading=true
@@ -30,8 +46,11 @@ console.log(this.loadedWeather)
 })
 }
 tocheckHourly(i:any){
-console.log(i)
+
+  console.log(i)
 this.weatherS.weatherSubject.next(i);
-this.router.navigate(['checkHourly'])
+this.router.navigate(['./checkHourly'])
+this.weatherS.WeatherStart.next([this.WeatherData,this.loadedWeather])
+
 }
 }
